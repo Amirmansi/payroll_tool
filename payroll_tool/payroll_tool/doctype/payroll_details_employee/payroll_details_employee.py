c70bfbd7469c7ed654acdabbc92db86f58a433aa
Amirmansi <163479758+Amirmansi@users.noreply.github.com>
2026-02-02 11:11:24 +0300

    Update copyright notice in payroll_details_employee.py

--- payroll_tool/payroll_tool/doctype/payroll_details_employee/payroll_details_employee.py
+++ payroll_tool/payroll_tool/doctype/payroll_details_employee/payroll_details_employee.py
@@ -1,7 +1,8 @@
-# Copyright (c) 2024, Your Company and contributors
+# Copyright (c) 2024, Amir Mansi and contributors
 # For license information, please see license.txt
 
 from frappe.model.document import Document
 
 class PayrollDetailsEmployee(Document):
     pass
+ENDOFPYTHON
